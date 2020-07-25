b29dd53cb81fc02a92962806a883063595caa566
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-25 22:10:57 +0300

    Tests with exception changed

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,34 +1,41 @@
 package ru.netology.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
 import ru.netology.domain.TShirt;
+import ru.netology.exception.NotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
     private ProductRepository repository = new ProductRepository();
-    private Book coreJava = new Book();
-    private TShirt tShirt = new TShirt();
+    private Product product = new Product(1, "product", 1000);
+    private Book book = new Book(2, "book", 2000, "author", 10, 2020);
+    private TShirt tShirt = new TShirt(3, "tshirt", 3000, "color", "size");
+
+
+    @BeforeEach
+    void setUp() {
+        repository.save(product);
+        repository.save(book);
+        repository.save(tShirt);
+    }
 
     @Test
-    public void shouldSaveOneItem(){
-        repository.save(coreJava);
-        repository.save(tShirt);
+    public void shouldRemoveByIdIfExist(){
+        repository.removeById(3);
 
-        Product[] expected = new Product[]{coreJava, tShirt};
+        Product[] expected = new Product[]{product, book};
         Product[] actual = repository.findAll();
 
         assertArrayEquals(expected, actual);
     }
 
-//    @Test
-//    public void shouldCastFromBookClass(){
-//        Product product = new Book();
-//        if (product instanceof Book) {
-//            Book book = (Book) product;
-//        }
-//    }
+    @Test
+    public void shouldRemoveByIdIfNotExist(){
+        assertThrows(NotFoundException.class, () -> repository.removeById(5));
+    }
 
 }
