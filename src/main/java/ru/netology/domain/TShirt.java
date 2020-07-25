fd2c2908a30a1076228771b74356e6a81451164b
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-25 21:26:24 +0300

    @Override for TShirt class added

--- src/main/java/ru/netology/domain/TShirt.java
+++ src/main/java/ru/netology/domain/TShirt.java
@@ -1,22 +1,23 @@
 package ru.netology.domain;
 
+import java.util.Objects;
+
 public class TShirt extends Product {
     private String color;
     private String size;
 
-    public String getColor() {
-        return color;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        TShirt tShirt = (TShirt) o;
+        return Objects.equals(color, tShirt.color) &&
+                Objects.equals(size, tShirt.size);
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), color, size);
     }
 }
